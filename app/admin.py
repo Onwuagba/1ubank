0821f65b7b83f3673836bcebceac098ce4ab15da
Kenenna Onwuagba <onwuagbakenenna@gmail.com>
2023-10-20 01:36:41 +0100

    feat: expose models to admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from app.models import Article, Currency, Provider
 
-# Register your models here.
+
+class DefaultAdmin(admin.ModelAdmin):
+    ordering = ("-updated_at",)
+
+
+@admin.register(Article)
+class ArticleAdmin(DefaultAdmin):
+    list_display = (
+        "article_no",
+        "article",
+        "provider",
+        "price",
+        "created_at",
+        "updated_at",
+    )
+    search_fields = [
+        "article__currency_id",
+        "provider__provider_no",
+        "article_no",
+    ]
+    readonly_fields = ("article_no",)
+
+
+class CurrencyAdmin(DefaultAdmin):
+    list_display = ("currency_id", "currency_name", "created_at", "updated_at")
+    search_fields = [
+        "currency_name",
+        "currency_id",
+    ]
+
+
+class ProviderAdmin(DefaultAdmin):
+    list_display = ("provider_no", "provider_name", "created_at", "updated_at")
+    search_fields = [
+        "provider_name",
+        "provider_no",
+    ]
+    readonly_fields = ("provider_no",)
+
+
+admin.site.register(Provider, ProviderAdmin)
+admin.site.register(Currency, CurrencyAdmin)
